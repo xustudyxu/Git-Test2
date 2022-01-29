38b627816e909cf5b1523691b006467a1e5d57d1
xustudyxu <1812903531@qq.com>
2022-01-29 16:23:42 +0800

    push commit

---
+++ src/main/java/com/frx01/git_/TestGit_.java
@@ -0,0 +1,12 @@
+package com.frx01.git_;
+
+/**
+ * @author frx
+ * @version 1.0
+ * @date 2022/1/29  16:22
+ */
+public class TestGit_ {
+    public static void main(String[] args) {
+        System.out.println("test push");
+    }
+}
